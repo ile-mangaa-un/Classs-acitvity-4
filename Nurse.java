076c4e53d416f699155740342fe22c6a753cd0c0
ile-mangaa-un <christopherchege21@gmail.com>
2026-03-30 09:49:18 +0300

    Add Nurse class extending Employee with Serializable

---
+++ Nurse.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+
+public class Nurse extends Employee implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String department;
+
+
+}
